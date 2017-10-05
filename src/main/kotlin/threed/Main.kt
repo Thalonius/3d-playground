b2e5680d0a29561484e1c797bf19b273a786f391
Peter Sswimbersky <peter.sswimbersky@mathconsult.co.at>
2017-10-05 12:03:08 +0200

    added Unit Tests for SceneTriangle

--- src/main/kotlin/threed/Main.kt
+++ src/main/kotlin/threed/Main.kt
@@ -5,6 +5,8 @@ import org.w3c.dom.HTMLCanvasElement
 import org.w3c.dom.HTMLDivElement
 import threed.example.drawExample
 import kotlin.browser.document
+import kotlin.test.*
+import spr5.test.TestingFixture
 
 
 fun main(args: Array<String>) {
@@ -14,6 +16,10 @@ fun main(args: Array<String>) {
     canvas.style.height = "100%"
 
     val webGlContext = createWebGLRenderingContext(canvas)
+    val testObject = TestingFixture()
+    testObject.TestObjectCreation()
+    testObject.TestTriangleColor()
+    testObject.TestConstructionPoints()
 
     container.appendChild(canvas)
 
